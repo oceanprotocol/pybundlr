0db31c97aa68b60a732972a2ca02ddee429ff297
juanpablovs <43534020+juanpablovs@users.noreply.github.com>
2022-12-07 17:39:54 +0100

    added and modified helper methods tests

--- pybundlr/test/test_1_helper_methods.py
+++ pybundlr/test/test_1_helper_methods.py
@@ -1,43 +1,65 @@
 import pytest
-from pybundlr import getBundlrNode, init
+from pybundlr import get_bundlr_node, init
 
 
-class TestGetBundlrNode:
+class Test_Get_Bundlr_Node:
 
-    def test_getBundlrNode_returns_str_on_valid_node(self):
+    def test_get_bundlr_node_returns_str_on_valid_node(self):
         valid_node = ['https://ubuntu.com/']
-        assert isinstance(getBundlrNode(valid_node), str)
+        assert isinstance(get_bundlr_node(valid_node), str)
 
-    def test_getBundlrNode_exits_if_there_are_no_valid_nodes(self):
+    def test_get_bundlr_node_exits_if_there_are_no_valid_nodes(self):
         invalid_node = ['https://gsdfanExampleofstdoesnotwork.sdsdf/']
         with pytest.raises(SystemExit):
-            getBundlrNode(invalid_node)
+            get_bundlr_node(invalid_node)
 
 
-class TestInit:
+class Test_Init:
 
     def test_init_returns_class_when_valid_paremeters_submitted(self):
         result = init(
-            currency_name='matic',
-            private_key='8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f')
+            currency_name = pytest.erc20_matic_currency_name,
+            private_key = pytest.erc20_matic_private_key,
+        )
         assert isinstance(result, object)
-
-    def test_init_returns_class_when_valid_paremeters_submitted_but_node_is_wrong(self):
-        invalid_node = ['https://exampleofstdoesnotwork.sdsdf/']
-        with pytest.raises(SystemExit):
-            init(
-            currency_name='matic',
-            private_key='8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f',
-            node=invalid_node)
+        assert result['uploader']['currencyConfig']['wallet'] == pytest.erc20_matic_private_key
 
     def test_init_returns_exception_if_currency_name_is_not_supported(self):
         with pytest.raises(Exception):
             init(
-            currency_name='maAAtic',
-            private_key='8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f')
+            currency_name = 'maAAtic',
+            private_key = pytest.erc20_matic_private_key)
 
     def test_init_returns_exception_if_private_key_is_incorrect(self):
         with pytest.raises(Exception):
             init(
-            currency_name='matic',
+            currency_name = pytest.erc20_matic_currency_name,
             private_key='--a4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f')
+
+    def test_init_returns_class_when_valid_paremeters_submitted_and_it_is_a_test_for_ERC20_token(self):
+        result = init(
+            currency_name = pytest.erc20_chainlink_currency_name,
+            private_key = pytest.erc20_chainlink_private_key,
+            is_a_test = {
+                'testNode': pytest.bundlr_test_node,
+                'providerUrl': pytest.erc20_chainlink_provider_url,
+                'contractAddress': pytest.erc20_chainlink_contract_address
+            })
+        assert isinstance(result, object)
+        assert result['uploader']['currencyConfig']['name'] == pytest.erc20_chainlink_currency_name
+        assert result['uploader']['currencyConfig']['providerUrl'] == pytest.erc20_chainlink_provider_url
+        assert result['uploader']['currencyConfig']['contractAddress'] == pytest.erc20_chainlink_contract_address
+
+    def test_init_returns_class_when_valid_paremeters_submitted_and_it_is_a_test_for_Arweave_token(self):
+        result = init(
+            currency_name = pytest.no_erc20_arweave_currency_name,
+            private_key = pytest.no_erc20_arweave_private_key,
+            is_a_test = {
+                'testNode': pytest.bundlr_test_node,
+                # 'providerUrl': pytest.erc20_chainlink_provider_url,
+                'contractAddress': pytest.no_erc20_arweave_contract_address
+            })
+        assert isinstance(result, object)
+        assert result['uploader']['currencyConfig']['name'] == pytest.no_erc20_arweave_currency_name
+        # assert result['uploader']['currencyConfig']['providerUrl'] == pytest.erc20_chainlink_provider_url
+        assert result['uploader']['currencyConfig']['contractAddress'] == pytest.no_erc20_arweave_contract_address
